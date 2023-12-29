7a8985f27385444677e52e62315b0d9a20fc3fcd
Gaël Reyrol <me@gaelreyrol.dev>
2023-12-29 09:02:39 +0100

    feat(github/ci): Add codecov and coveralls to workflow (#16)

--- tests/Unit/OpenTelemetry/Exporter/OtlpExporterOptionsTest.php
+++ tests/Unit/OpenTelemetry/Exporter/OtlpExporterOptionsTest.php
@@ -21,7 +21,7 @@ class OtlpExporterOptionsTest extends TestCase
 
         self::assertSame(OtlpExporterFormatEnum::Json, $options->getFormat());
         self::assertSame([
-            'User-Agent' => 'OTel OTLP Exporter PHP/1.0.3, Symfony OTEL Bundle',
+            'User-Agent' => 'OTel OTLP Exporter PHP/1.0.4, Symfony OTEL Bundle',
         ], $options->getHeaders());
         self::assertSame(OtlpExporterCompressionEnum::None, $options->getCompression());
         self::assertSame(.10, $options->getTimeout());
@@ -64,7 +64,7 @@ class OtlpExporterOptionsTest extends TestCase
             ['headers' => ['X-Foo' => 'Bar']],
             fn (OtlpExporterOptions $options) => self::assertSame([
                 'X-Foo' => 'Bar',
-                'User-Agent' => 'OTel OTLP Exporter PHP/1.0.3, Symfony OTEL Bundle',
+                'User-Agent' => 'OTel OTLP Exporter PHP/1.0.4, Symfony OTEL Bundle',
             ], $options->getHeaders()),
         ];
 
