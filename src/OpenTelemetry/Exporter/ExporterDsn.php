cc27a722f06f154e61cc1b7a561923a597b5817d
Gaël Reyrol <me@gaelreyrol.dev>
2024-01-04 12:07:14 +0100

    refactor(otel/exporter): use Zenstruck\Dsn&Uri for ExporterDsn

--- src/OpenTelemetry/Exporter/ExporterDsn.php
+++ src/OpenTelemetry/Exporter/ExporterDsn.php
@@ -2,79 +2,67 @@
 
 namespace FriendsOfOpenTelemetry\OpenTelemetryBundle\OpenTelemetry\Exporter;
 
+use Zenstruck\Dsn;
+use Zenstruck\Uri;
+
 final class ExporterDsn
 {
-    public function __construct(
-        private readonly string $scheme,
-        private readonly string $host,
-        private readonly ?string $user = null,
-        #[\SensitiveParameter]
-        private readonly ?string $password = null,
-        private readonly ?int $port = null,
-        private readonly ?string $path = null,
-        /**
-         * @var array<string, mixed>
-         */
-        private readonly array $options = []
+    private function __construct(
+        private readonly Uri $uri,
     ) {
     }
 
     public static function fromString(#[\SensitiveParameter] string $dsn): self
     {
-        if (false === $parsedDsn = parse_url($dsn)) {
-            throw new \InvalidArgumentException('The DSN is invalid.');
+        try {
+            $parsedDsn = Dsn::parse($dsn);
+        } catch (Dsn\Exception\UnableToParse $exception) {
+            throw new \InvalidArgumentException('The DSN is invalid.', previous: $exception);
         }
 
-        if (!isset($parsedDsn['scheme'])) {
+        if (false === $parsedDsn instanceof Uri) {
+            throw new \InvalidArgumentException('The DSN is not an Uri.');
+        }
+
+        if (true === $parsedDsn->scheme()->isEmpty()) {
             throw new \InvalidArgumentException('The DSN must contain a scheme.');
         }
 
-        if (!isset($parsedDsn['host'])) {
+        if (true === $parsedDsn->host()->isEmpty()) {
             throw new \InvalidArgumentException('The DSN must contain a host (use "default" by default).');
         }
 
-        $user = '' !== ($parsedDsn['user'] ?? '') ? urldecode($parsedDsn['user']) : null;
-        $password = '' !== ($parsedDsn['pass'] ?? '') ? urldecode($parsedDsn['pass']) : null;
-        $port = $parsedDsn['port'] ?? null;
-        $path = $parsedDsn['path'] ?? null;
-        parse_str($parsedDsn['query'] ?? '', $query);
-
-        return new self($parsedDsn['scheme'], $parsedDsn['host'], $user, $password, $port, $path, $query);
+        return new self($parsedDsn);
     }
 
     public function getScheme(): string
     {
-        return $this->scheme;
+        return $this->uri->scheme()->toString();
     }
 
     public function getHost(): string
     {
-        return $this->host;
+        return $this->uri->host()->toString();
     }
 
     public function getUser(): ?string
     {
-        return $this->user;
+        return $this->uri->username();
     }
 
     public function getPassword(): ?string
     {
-        return $this->password;
+        return $this->uri->password();
     }
 
     public function getPath(): ?string
     {
-        return $this->path;
+        return $this->uri->path()->isEmpty() ? null : $this->uri->path()->toString();
     }
 
     public function getPort(int $default = null): ?int
     {
-        return $this->port ?? $default;
-    }
-
-    public function getOption(string $key, mixed $default = null): mixed
-    {
-        return $this->options[$key] ?? $default;
+        return $this->uri->port() ?? $default;
     }
 
     /**
@@ -82,7 +70,7 @@ final class ExporterDsn
      */
     private function parseScheme(): array
     {
-        return explode('+', $this->getScheme(), 2);
+        return $this->uri->scheme()->segments();
     }
 
     public function getExporter(): string
