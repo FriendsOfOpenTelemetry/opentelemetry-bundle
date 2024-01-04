4331f9eccd2526be9e70387215cf79a570917145
Gaël Reyrol <me@gaelreyrol.dev>
2024-01-04 12:21:29 +0100

    refactor(otel/exporter): Use Zenstruck\Dsn&Uri for ExporterDsn (#22)

--- tests/Unit/OpenTelemetry/Exporter/ExporterDsnTest.php
+++ tests/Unit/OpenTelemetry/Exporter/ExporterDsnTest.php
@@ -10,62 +10,56 @@ use PHPUnit\Framework\TestCase;
  */
 class ExporterDsnTest extends TestCase
 {
-    public function testGetOption(): void
-    {
-        $options = ['format' => 'json', 'nullable' => null];
-        $dsn = new ExporterDsn(scheme: 'http+otlp', host: 'localhost', options: $options);
-
-        self::assertSame('json', $dsn->getOption('format'));
-        self::assertSame('default', $dsn->getOption('nullable', 'default'));
-        self::assertSame('default', $dsn->getOption('not_existent_property', 'default'));
-    }
-
     /**
      * @dataProvider fromStringProvider
      *
-     * @param array{transport?: string, exporter?: string} $explodedScheme
+     * @param array{transport?: string, exporter?: string} $expected
      */
-    public function testFromString(string $string, ExporterDsn $dsn, array $explodedScheme): void
+    public function testFromString(ExporterDsn $dsn, array $expected): void
     {
-        self::assertEquals($dsn, ExporterDsn::fromString($string));
-        self::assertEquals($explodedScheme, [
+        self::assertEquals($expected, [
             'transport' => $dsn->getTransport(),
             'exporter' => $dsn->getExporter(),
+            'user' => $dsn->getUser(),
+            'password' => $dsn->getPassword(),
+            'host' => $dsn->getHost(),
+            'port' => $dsn->getPort(),
+            'path' => $dsn->getPath(),
         ]);
     }
 
     /**
-     * @return iterable<string, array{0: string, 1: ExporterDsn, 2: array{transport: ?string, exporter: string}}>
+     * @return iterable<string, array{0: ExporterDsn, 1: array{
+     *     transport: ?string,
+     *     exporter: string,
+     *     user: ?string,
+     *     password: ?string,
+     *     host: string,
+     *     port: ?int,
+     *     path: ?string
+     * }}>
      */
     public static function fromStringProvider(): iterable
     {
         yield 'gRPC Transport, OTLP Exporter' => [
-            'grpc+otlp://localhost:4317',
-            new ExporterDsn('grpc+otlp', 'localhost', null, null, 4317),
-            ['transport' => 'grpc', 'exporter' => 'otlp'],
+            ExporterDsn::fromString('grpc+otlp://localhost:4317'),
+            ['transport' => 'grpc', 'exporter' => 'otlp', 'user' => null, 'password' => null, 'host' => 'localhost', 'port' => 4317, 'path' => null],
         ];
-        yield 'HTTP Transport, OTLP Exporter with JSON Content-Type, Compression GZIP' => [
-            'http+otlp://localhost:4318/v1/traces?content-type=application/json&compression=gzip',
-            new ExporterDsn('http+otlp', 'localhost', null, null, 4318, '/v1/traces', [
-                'content-type' => 'application/json',
-                'compression' => 'gzip',
-            ]),
-            ['transport' => 'http', 'exporter' => 'otlp'],
+        yield 'HTTP Transport, OTLP Exporter' => [
+            ExporterDsn::fromString('http+otlp://localhost:4318/v1/traces'),
+            ['transport' => 'http', 'exporter' => 'otlp', 'user' => null, 'password' => null, 'host' => 'localhost', 'port' => 4318, 'path' => '/v1/traces'],
         ];
         yield 'HTTP Transport, Zipkin Exporter with basic authentication' => [
-            'http+zipkin://user:password@localhost:9411/api/v2/spans',
-            new ExporterDsn('http+zipkin', 'localhost', 'user', 'password', 9411, '/api/v2/spans'),
-            ['transport' => 'http', 'exporter' => 'zipkin'],
+            ExporterDsn::fromString('http+zipkin://user:password@localhost:9411/api/v2/spans'),
+            ['transport' => 'http', 'exporter' => 'zipkin', 'user' => 'user', 'password' => 'password', 'host' => 'localhost', 'port' => 9411, 'path' => '/api/v2/spans'],
         ];
         yield 'Stream Transport, Console Exporter' => [
-            'stream+console://default',
-            new ExporterDsn('stream+console', 'default'),
-            ['transport' => 'stream', 'exporter' => 'console'],
+            ExporterDsn::fromString('stream+console://default'),
+            ['transport' => 'stream', 'exporter' => 'console', 'user' => null, 'password' => null, 'host' => 'default', 'port' => null, 'path' => null],
         ];
         yield 'Memory Exporter' => [
-            'memory://default',
-            new ExporterDsn('memory', 'default'),
-            ['transport' => null, 'exporter' => 'memory'],
+            ExporterDsn::fromString('memory://default'),
+            ['transport' => null, 'exporter' => 'memory', 'user' => null, 'password' => null, 'host' => 'default', 'port' => null, 'path' => null],
         ];
     }
 
@@ -97,6 +91,11 @@ class ExporterDsnTest extends TestCase
         yield [
             'file:///some/path',
             'The DSN must contain a host (use "default" by default).',
+        ];
+
+        yield [
+            'mailto:john@doe.com',
+            'The DSN is not an Uri.',
         ];
     }
 }
