162f309e92905bba188f1f0b57878978d02ec2fa
Gaël Reyrol <gael.reyrol@prestashop.com>
2025-04-09 09:04:05 +0200

    fix(Instrumentation/Console): empty exclude config should not exclude all commands

---
+++ tests/Functional/Instrumentation/Console/ConsoleEmptyExcludesTracingTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace FriendsOfOpenTelemetry\OpenTelemetryBundle\Tests\Functional\Instrumentation\Console;
+
+use App\Kernel;
+use FriendsOfOpenTelemetry\OpenTelemetryBundle\Tests\Functional\TracingTestCaseTrait;
+use OpenTelemetry\SDK\Trace\StatusData;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\ApplicationTester;
+use Zalas\PHPUnit\Globals\Attribute\Env;
+
+#[Env('KERNEL_CLASS', Kernel::class)]
+#[Env('APP_ENV', 'empty_excludes')]
+class ConsoleEmptyExcludesTracingTest extends KernelTestCase
+{
+    use TracingTestCaseTrait;
+
+    public function testSuccess(): void
+    {
+        $kernel = self::bootKernel();
+
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
+
+        $tester = new ApplicationTester($application);
+
+        $tester->run(['command' => 'traceable:auto-command']);
+        self::assertSame(0, $tester->getStatusCode());
+
+        self::assertSpansCount(1);
+
+        $mainSpan = self::getSpans()[0];
+        self::assertSpanName($mainSpan, 'traceable:auto-command');
+        self::assertSpanStatus($mainSpan, StatusData::ok());
+        self::assertSpanAttributes($mainSpan, [
+            'code.function.name' => 'execute',
+            'code.namespace' => 'App\Command\Traceable\AutoCommand',
+            'symfony.console.exit_code' => 0,
+        ]);
+        self::assertSpanEventsCount($mainSpan, 0);
+    }
+}
